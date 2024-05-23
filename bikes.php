342e60e7176612f6e2369f12f2e05ac2341e2a93
RAC_RSIT <rahat.ahmed@rootsoftit.com>
2024-05-23 14:05:39 +0600

    DAY01: first commit

---
+++ bikes.php
@@ -0,0 +1,36 @@
+<?php 
+
+$bikes = [
+    [
+        "brand" => "Bajaj", 
+        "model" => "Avenger Street",
+        "cc" => 160, 
+        "color" => "Black", 
+        "registration" => "SYL-128565"
+    ],
+    [
+        "brand" => "Hero", 
+        "model" => "Ignitor Techno",
+        "cc" => 125, 
+        "color" => "Red & Black", 
+        "registration" => "SYL-122263"
+    ],
+    [
+        "brand" => "TVS", 
+        "model" => "Metro ES",
+        "cc" => 125, 
+        "color" => "Black & Blue", 
+        "registration" => "SYL-129687"
+    ],
+    [
+        "brand" => "Bajaj", 
+        "model" => "Discover",
+        "cc" => 125, 
+        "color" => "Green", 
+        "registration" => "DHA-127463"
+    ]
+];
+
+
+// return $bikes;
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    include "bikes.php";
+    echo "<pre>";
+    print_r($bikes);
+    echo "</pre>";
+    
+    
+?>
