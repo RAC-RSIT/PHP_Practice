4d401ad9e89c83426f8e1c60ed2299390195d6f8
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2024-05-25 15:19:24 +0600

    DAY02: data_types

---
+++ data_types.php
@@ -0,0 +1,27 @@
+<?php 
+    /* PHP supports these data types- 
+        1. String
+        2. Integer 
+        3. Float 
+        4. Boolean 
+        5. Array 
+        6. Object 
+        7. Null 
+        8. Resource 
+    */
+
+    // getting the data type 
+    $x = 5; 
+    var_dump($x);
+
+    $x = 5.003; 
+    var_dump($x);
+
+    $x = "Sylhet"; 
+    var_dump($x);
+
+    $x = true; 
+    var_dump($x); 
+
+    // var_dump() gives us information about the variable 
+?>
