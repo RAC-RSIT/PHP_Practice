0474ffce185add25fe4ea9a1a3042b52e3cf4681
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2024-05-25 15:09:12 +0600

    DAY02: echo_and_print

---
+++ echo_and_print.php
@@ -0,0 +1,11 @@
+<?php 
+    // echo doesn't have a return value while print returns 1 
+    // echo can take mulitple arguments but print can accept only one argument 
+    // echo is faster then print 
+    // both echo and print can be used with or without parenthesis 
+
+    // example: 
+    $message = "As salamu alaikum";
+    echo "<h1> $message </h1>"; 
+    print "<h1> $message </h1>"; 
+?>
