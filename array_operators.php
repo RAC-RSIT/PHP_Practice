ee87fc7fb69ffbc4b759ae4de5d7322448a1c215
RAC_RSIT <rahat.ahmed@rootsoftit.com>
2024-05-27 10:19:22 +0600

    DAY04: array_operators

---
+++ array_operators.php
@@ -0,0 +1,15 @@
+<?php 
+    // array operators are- +, ==, ===, !=, !==
+    
+    $x = array("a" => "red", "b" => "green");
+    $y = array("c" => "blue", "d" => "yellow"); 
+
+    print_r($x + $y); // union of $x and $y 
+    echo "<br>"; 
+    if($x == $y){
+    	echo "TRUE";
+    }
+    else{
+    	echo "FALSE";
+    }
+?>  
